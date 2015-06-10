055b49af7975f25cb88b29e33dfd5742d9493714
Mentu Singh <singhc@wit.edu>
2015-06-10 14:15:10 -0400

    Lab4 Done

--- src/brainfuck.cpp
+++ src/brainfuck.cpp
@@ -1,8 +1,6 @@
-/*
+/* Chaskarandeep Singh
 = Brainfuck
-
 If you have gcc:
-
 ----
 g++ -o brainfuck.exe brainfuck.cpp
 brainfuck.exe helloworld.bf
@@ -24,7 +22,8 @@ typedef enum {
     SHIFT_LEFT, // <
     SHIFT_RIGHT, // >
     INPUT, // ,
-    OUTPUT // .
+    OUTPUT, // .
+	ZERO // [-] or [+]
 } Command;
 
 // Forward references. Silly C++!
@@ -58,7 +57,8 @@ class Node {
 class CommandNode : public Node {
     public:
         Command command;
-        CommandNode(char c) {
+		int count;
+        CommandNode(char c, int n) {
             switch(c) {
                 case '+': command = INCREMENT; break;
                 case '-': command = DECREMENT; break;
@@ -66,7 +66,9 @@ class CommandNode : public Node {
                 case '>': command = SHIFT_RIGHT; break;
                 case ',': command = INPUT; break;
                 case '.': command = OUTPUT; break;
+				case 'z': command = ZERO; break;
             }
+			count = n;
         }
         void accept (Visitor * v) {
             v->visit(this);
@@ -90,6 +92,9 @@ class Loop : public Container {
         }
 };
 
+
+
+
 /**
  * Program is the root of a Brainfuck program abstract syntax tree.
  * Because Brainfuck is so primitive, the parse tree is the abstract syntax tree.
@@ -105,33 +110,54 @@ class Program : public Container {
  * Read in the file by recursive descent.
  * Modify as necessary and add whatever functions you need to get things done.
  */
-void parse(fstream & file, Container * container) 
-{
-    char c;
-	file >> c;
-	
-    if (c=='+' || c=='-' || c=='<' || c=='>' || c== ',' || c=='.')
-	{
-    container->children.push_back(new CommandNode(c));
+void parse(fstream & file, Container * container) {
+	Loop * program; // Our loop object
+	char c;
+	int count;
+    // How to insert a node into the container
+
+	while (file >> c) {
+		count = 1; // reset count
+		//command case
+		if(c == '+' || c == '-' || c == '<' || c == '>' || c == ',' || c == '.'){
+			while(((char)file.peek()) == c){ // Squash down repeats, aka +++ -> Node with + and count = 3
+				file >> c; // move file pointer
+				count++; // increase
+			}
+			container->children.push_back(new CommandNode(c,count)); //add our node to the tree
+		}
+		else if(c == '['){  // Loop case
+			program = new Loop(); // Create new loop object
+			parse(file, program); // Parse the inside of the loop
+			if (program->children.size() == 1) { // If we have only one object inside the loop, check for special cases.
+				CommandNode* child = dynamic_cast<CommandNode*>(program->children.front());  // 
+				if (child->command == INCREMENT || child->command == DECREMENT) { // If loop is either [+] or [-]
+					container->children.push_back(new CommandNode('z',1)); // Add special ZERO node.
+					delete program; // Avoid lingering loop objects
+				} else {
+					container->children.push_back(program);	// Normal non-special loop with size one.
+				}
+			}else{
+				container->children.push_back(program);	 //Add a normal non-special loop to the tree.
+			}
+		}
+		else if (c == ']') { //end of loop object
+			return;
+		}
+		
 	}
-	// How to peek at the next character
-    c = (char)file.peek();
-	
-	if(c=='[')
-	{
-		Loop my_loop;
-		parse(file, & my_loop);
-		container->children.push_back(new Loop(my_loop));
-		file >> c;
-	}
-	
-	c=(char)file.peek();
-	if(c=='+' || c=='-' || c=='<' || c=='>' || c== ',' || c=='.')
-	{
-		parse(file, container);
-	}
-    
 }
+
+
+/*
+Program -> Sequence
+Sequence -> Command Sequence
+Sequence -> Loop Sequence
+Sequence -> any other character, ignore (treat as a comment)
+Sequence -> "" (empty string)
+Command -> '+' | '-' | '<' | '>' | ',' | '.'
+Loop -> '[' Sequence ']'
+*/
 
 /**
  * A printer for Brainfuck abstract syntax trees.
@@ -141,14 +167,17 @@ void parse(fstream & file, Container * container)
 class Printer : public Visitor {
     public:
         void visit(const CommandNode * leaf) {
-            switch (leaf->command) {
-                case INCREMENT:   cout << '+'; break;
-                case DECREMENT:   cout << '-'; break;
-                case SHIFT_LEFT:  cout << '<'; break;
-                case SHIFT_RIGHT: cout << '>'; break;
-                case INPUT:       cout << ','; break;
-                case OUTPUT:      cout << '.'; break;
-            }
+		for (int i = 0; i < leaf->count; i++){
+				switch (leaf->command) {
+					case INCREMENT:   cout << '+'; break;
+					case DECREMENT:   cout << '-'; break;
+					case SHIFT_LEFT:  cout << '<'; break;
+					case SHIFT_RIGHT: cout << '>'; break;
+					case INPUT:       cout << ','; break;
+					case OUTPUT:      cout << '.'; break;
+					case ZERO:		  cout << 'z'; break;
+				}
+			}
         }
         void visit(const Loop * loop) {
             cout << '[';
@@ -165,90 +194,101 @@ class Printer : public Visitor {
         }
 };
 
+class JavaCompiler : public Visitor {
+    public:
+        void visit(const CommandNode * leaf) {
+			for (int i = 0; i < leaf->count; i++){
+					switch (leaf->command) {
+						case INCREMENT:   cout << "array[pointer]++;\n"; break;
+						case DECREMENT:   cout << "array[pointer]--;\n"; break;
+						case SHIFT_LEFT:  cout << "pointer--;\n"; break;
+						case SHIFT_RIGHT: cout << "pointer++;\n"; break;
+						case INPUT:       cout << "array[pointer] = (byte)System.in.read();\n"; break;
+						case OUTPUT:      cout << "System.out.print((char)array[pointer]);\n"; break;
+						case ZERO:		  cout << "array[pointer]=0;\n"; break;
+					}
+			}
+        }
+        void visit(const Loop * loop) {
+            cout << "while (array[pointer] == 1){ \n";
+            for (vector<Node*>::const_iterator it = loop->children.begin(); it != loop->children.end(); ++it) {
+                (*it)->accept(this);
+            }
+            cout << "}\n";
+        }
+        void visit(const Program * program) {
+			cout << "import java.util.Scanner;\n";
+			cout << "import java.io.IOException;\n\n";
+			cout << "public class Default {\n";
+			cout << "public static void main(String[] args) throws IOException {\n";
+			cout << "Scanner input = new Scanner(System.in);\n";
+			cout << "byte[] array = new byte[30000];\n";
+			cout << "int pointer = 0;\n";
+            for (vector<Node*>::const_iterator it = program->children.begin(); it != program->children.end(); ++it) {
+                (*it)->accept(this);
+            }
+			cout << "}\n";
+            cout << "}\n";
+        }
+};
+
 class Interpreter : public Visitor {
     char memory[30000];
     int pointer;
     public:
         void visit(const CommandNode * leaf) {
-            switch (leaf->command) {
-                case INCREMENT:
-					memory[pointer]++;
-                    break;
-                case DECREMENT:
-					memory[pointer]--;
-                    break;
-                case SHIFT_LEFT:
-					pointer--;
-                    break;
-                case SHIFT_RIGHT:
-					pointer++;
-                    break;
-                case INPUT:
-					cin.get(memory[pointer]);
-                    break;
-                case OUTPUT:
-					cout << memory[pointer];
-                    break;
-            }
+			for (int i = 0; i < leaf->count; i++){
+				switch (leaf->command) {
+					case INCREMENT:
+						memory[pointer]++;
+						break;
+					case DECREMENT:
+						memory[pointer]--;
+						break;
+					case SHIFT_LEFT:
+						pointer--;
+						break;
+					case SHIFT_RIGHT:
+						pointer++;
+						break;
+					case INPUT:
+						cin.get(memory[pointer]);
+						break;
+					case OUTPUT:
+						cout << memory[pointer];
+						break;
+					case ZERO:
+						memory[pointer]=0;
+						break;
+				}
+			}
         }
         void visit(const Loop * loop) {
-			while ( memory[pointer] ){
-            for (vector<Node*>::const_iterator it = loop->children.begin(); it != loop->children.end(); ++it) {
-                (*it)->accept(this);
-            }
-
+			while(memory[pointer]){
+				for (vector<Node*>::const_iterator it = loop->children.begin(); it != loop->children.end(); ++it) {
+					(*it)->accept(this);
+				}
 			}
         }
         void visit(const Program * program) {
             // zero init the memory array
-			for(int i=0; i <=30000; i++)
-			{
-				memory[i]=0;
-			}
             // set pointer to zero
-			pointer=0;
+			for(int i =0; i <= 30000; i++){
+				memory[i] = 0;
+			}
+			pointer = 0;
+
             for (vector<Node*>::const_iterator it = program->children.begin(); it != program->children.end(); ++it) {
                 (*it)->accept(this);
             }
         }
 };
-
-class CCompiler : public Visitor {
-    public:
-        void visit(const CommandNode * leaf) {
-            switch (leaf->command) {
-                case INCREMENT:   cout << "++*ptr;\n"; break;
-                case DECREMENT:   cout << "--*ptr;\n"; break;
-                case SHIFT_LEFT:  cout << "--ptr;\n"; break;
-                case SHIFT_RIGHT: cout << "++ptr;\n"; break;
-                case INPUT:       cout << "*ptr=getchar();\n"; break;
-                case OUTPUT:      cout << "putchar(*ptr);\n"; break;
-            }
-        }
-        void visit(const Loop * loop) {
-            cout << "while (*ptr) {\n";
-            for (vector<Node*>::const_iterator it = loop->children.begin(); it != loop->children.end(); ++it) {
-                (*it)->accept(this);
-            }
-            cout << "}\n";
-        }
-        void visit(const Program * program) {
-            cout << "#include <stdio.h>\n";
-            cout << "char array[30000] = {0};\n";
-            cout << "char *ptr=array;\n";
-            cout << "int main(int argc, char ** argv) {\n";
-            for (vector<Node*>::const_iterator it = program->children.begin(); it != program->children.end(); ++it) {
-                (*it)->accept(this);
-            }
-            cout << "}\n";
-        }
-};
-
 
 int main(int argc, char *argv[]) {
     fstream file;
     Program program;
-    CCompiler printer;
+    Printer printer;
+	JavaCompiler compiler;
     Interpreter interpreter;
     if (argc == 1) {
         cout << argv[0] << ": No input files." << endl;
@@ -256,8 +296,9 @@ int main(int argc, char *argv[]) {
         for (int i = 1; i < argc; i++) {
             file.open(argv[i], fstream::in);
             parse(file, & program);
-            program.accept(&printer);
-//            program.accept(&interpreter);
+         //  program.accept(&printer);
+           program.accept(&interpreter);
+		 //	program.accept(&compiler);
             file.close();
         }
     }
